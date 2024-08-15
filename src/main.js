1e7e5949909e0ed20dcae4f8fb4a79abfc3d4dc7
mkihato <mlamaar9@gmail.com>
2024-08-15 12:15:45 +0300

    thur commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 (function() {
-    var vertoHandle, vertoCallbacks;
+    var vertoHandle, vertoCallbacks,currentCall;
   
     $.verto.init({}, bootstrap);
   
     function bootstrap(status) {
       vertoHandle = new jQuery.verto({
         // The params...
-        login: '[USER]@[YOUR_FREESWITCH_SERVER_DOMAIN]',
-    passwd: '[PASSWORD]',
+        login: '1005@tenant1.telvoip.io',
+    passwd: '1005@telvoip$$',
     // As configured in verto.conf.xml on the server.
-    socketUrl: 'wss://[YOUR_FREESWITCH_SERVER_DOMAIN]:8082',
+    socketUrl: 'wss://tenant1.telvoip.io:8082',
     // TODO: Where is this file, on the server? What is the base path?
     ringFile: 'sounds/bell_ring2.wav',
     // STUN/TURN server config, more than one is allowed.
@@ -17,7 +17,7 @@
     // false disables STUN, true uses the default Google STUN servers.
     iceServers: [
       {
-        url: 'stun:[YOUR_STUN_SERVER]',
+        url: 'stun:stun.freeswitch.org]',
       },
     ],
     // These can be set per-call as well as per-login. They can also be set to
@@ -56,11 +56,14 @@
     // other purposes.
     //sessid: sessid,
       }, vertoCallbacks);
+
+      document.getElementById("make-call").addEventListener("click", makeCall);
     };
   
     vertoCallbacks = {
       onWSLogin: onWSLogin,
-      onWSClose: onWSClose
+      onWSClose: onWSClose,
+      onDialogState: onDialogState,
     };
   
     function onWSLogin(verto, success) {
@@ -70,4 +73,49 @@
     function onWSClose(verto, success) {
       console.log('onWSClose', success);
     };
+    function makeCall() {
+
+      currentCall = vertoHandle.newCall({
+        // Extension to dial.
+        destination_number: '1002',
+        caller_id_name: 'Extension 1002',
+        caller_id_number: '1001',
+        outgoingBandwidth: 'default',
+        incomingBandwidth: 'default',
+        // Enable stereo audio.
+        useStereo: true,
+        // Set to false to disable inbound video.
+        useVideo: true,
+        // You can pass any application/call specific variables here, and they will
+        // be available as a dialplan variable, prefixed with 'verto_dvar_'.
+        userVariables: {
+          // Shows up as a 'verto_dvar_email' dialplan variable.
+          email: 'test@test.com'
+        },
+        // Use a dedicated outbound encoder for this user's video.
+        // NOTE: This is generally only needed if the user has some kind of
+        // non-standard video setup, and is not recommended to use, as it
+        // dramatically increases the CPU usage for the conference.
+        dedEnc: false,
+        // Example of setting the devices per-call.
+        //useMic: 'any',
+        //useSpeak: 'any',
+      });
+    };
+    function onDialogState(d) {
+      switch (d.state.name) {
+        case "trying":
+          break;
+        case "answering":
+          break;
+        case "active":
+          break;
+        case "hangup":
+          log("Call ended with cause: " + d.cause);
+          break;
+        case "destroy":
+          // Some kind of client side cleanup...
+          break;
+      }
+    }
   })();
